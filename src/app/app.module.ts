16a96866e71a5ee0022cd64dc25c24e792b58298
AzureAD\AbhishekKumar <abhishek@monocept.com>
2019-06-15 19:40:29 +0530

    removed extra files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ErrorComponent } from './error.component';
-import { AppRoutingModule } from './routing.module';
-
 
 @NgModule({
   declarations:  [
-    AppComponent,
-    ErrorComponent
+    AppComponent
   ],
   imports: [
-    AppRoutingModule,
     BrowserModule,
     FormsModule,
     HttpClientModule
